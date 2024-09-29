8764da7fcca756345fca1c33b758e31162b686a3
felipehscherer <felipe.scherepento@gmail.com>
2024-09-29 19:41:10 -0300

    estilização da page de login

--- web-frontend/src/pages/Login.js
+++ web-frontend/src/pages/Login.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import axios from '../services/axiosConfig';
+import './Login.css'; 
+import logoImage from '../assets/logo.jpg'; 
 
 function Login() {
   const [email, setEmail] = useState('');
@@ -19,23 +21,34 @@ function Login() {
   };
 
   return (
-    <form onSubmit={handleLogin}>
-      <input
-        type="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        placeholder="Email"
-        required
-      />
-      <input
-        type="password"
-        value={password}
-        onChange={(e) => setSenha(e.target.value)}
-        placeholder="Senha"
-        required
-      />
-      <button type="submit">Entrar</button>
-    </form>
+    <div className="login-container">
+      <form className="login-box" onSubmit={handleLogin}>
+        <img src={logoImage} alt="Logo" className="login-logo" />
+        <p className="login-title">Faça seu login</p>
+
+        <label htmlFor="email" className="login-label">Digite seu email</label>
+        <input
+          id="email"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+          required
+        />
+
+        <label htmlFor="password" className="login-label">Digite sua senha</label>
+        <input
+          id="password"
+          type="password"
+          value={password}
+          onChange={(e) => setSenha(e.target.value)}
+          placeholder="Senha"
+          required
+        />
+
+        <button type="submit">Entrar</button>
+      </form>
+    </div>
   );
 }
 
