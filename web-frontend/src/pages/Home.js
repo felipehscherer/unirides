eb083379265ba3bd682948a4bdfeb48f803a2936
EduardoPaim5 <epaimmmv@gmail.com>
2024-10-07 03:24:13 -0300

    ajustes no cadastro de motorista

--- web-frontend/src/pages/Home.js
+++ web-frontend/src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; // Importa o Link
 import './styles/Home.css';
-import logoImage from '../assets/logo.jpg'; 
+import logoImage from '../assets/logo.jpg';
 
 function Home() {
   return (
@@ -9,6 +10,11 @@ function Home() {
         <img src={logoImage} alt="Logo" className="home-logo" />
         <p className="home-title">Bem-vindo à página inicial!</p>
         <p className="home-text">Você está logado com sucesso.</p>
+
+        {/* Botão para navegar para a página de cadastro de motorista */}
+        <Link to="/cadastromotorista">
+          <button className="cadastro-button">Cadastrar Motorista</button>
+        </Link>
       </div>
     </div>
   );
