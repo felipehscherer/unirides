3a719dbc79852ca9647e7a0e5e749d8afd55ee33
DiogoGiordano <diogogiordano.aluno@unipampa.edu.br>
2024-10-14 14:26:15 -0300

    Adicionado a pagina de visualizacao dos veiculos (Erro no commit anterior)

---
+++ web-frontend/src/pages/ApresentarVeiculos.js
@@ -0,0 +1,69 @@
+import React, {useState, useEffect} from 'react';
+import axios from '../services/axiosConfig';
+import './styles/ApresentarVeiculos.css';
+import {useNavigate} from 'react-router-dom';
+
+function ApresentarVeiculos() {
+    const [vehicles, setVehicles] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [email, setEmail] = useState('');
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        async function fetchVehicles() {
+            try {
+                const response = await axios.get('/user/profile');
+                const data = response.data;
+                setEmail(data.email);
+
+                if (data.email) {
+                    const responseVehicle = await axios.get(`/vehicle/get/byUserEmail/${data.email}`);
+                    const vehiclesData = responseVehicle.data;
+
+                    setVehicles(Array.isArray(vehiclesData) ? vehiclesData : []);
+                }
+
+            } catch (error) {
+                console.error('Erro ao buscar dados do usuário:', error);
+                if (error.response && error.response.status === 403) {
+                    console.error('Acesso negado - erro 403.');
+                } else if (error.response && error.response.status === 401) {
+                    navigate('/home');
+                } else {
+                    setVehicles([]);
+                }
+            } finally {
+                setLoading(false);
+            }
+        };
+        fetchVehicles();
+    }, [email, navigate]);
+
+    if (loading) {
+        return <p>Carregando veículos...</p>;
+    }
+
+    return (
+        <ul className="vehicle-list">
+
+            {Array.isArray(vehicles) && vehicles.length > 0 ? (
+                vehicles.map((vehicle, index) => (
+                    <li key={index} className="vehicle-item">
+                        <p><strong>Marca:</strong> {vehicle.brand}</p>
+                        <p><strong>Modelo:</strong> {vehicle.model}</p>
+                        <p><strong>Cor:</strong> {vehicle.color}</p>
+                        <p><strong>Placa:</strong> {vehicle.plate}</p>
+                        <button className="vehicle-detailsBtn">📄Detalhes</button>
+                        <button className="vehicle-UpdateBtn">✏️ Editar</button>
+                        <button className="vehicle-deleteBtn">🗑️ Editar</button>
+                    </li>
+                ))
+            ) : (
+                <p>Nenhum veículo encontrado.</p>
+            )}
+        </ul>
+    );
+}
+
+export default ApresentarVeiculos;
