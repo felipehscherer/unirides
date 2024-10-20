d22edc9f4ed54f8fbec16e4493cdf3d3a5632816
felipehscherer <felipe.scherepento@gmail.com>
2024-10-20 18:34:52 -0300

    feat: criação da classe ride

---
+++ login-auth-api/src/main/java/br/com/unirides/loginauthapi/domain/ride/Ride.java
@@ -0,0 +1,47 @@
+package br.com.unirides.loginauthapi.domain.ride;
+
+import br.com.unirides.loginauthapi.domain.driver.Driver;
+import br.com.unirides.loginauthapi.domain.driver.Vehicle;
+import br.com.unirides.loginauthapi.domain.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Table(name = "rides")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Ride {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "driver_id", nullable = false)
+    private Driver driver;
+
+    @ManyToOne
+    @JoinColumn(name = "vehicle_id", nullable = false)
+    private Vehicle vehicle;
+
+    @ManyToMany
+    @JoinTable(
+            name = "ride_passengers",
+            joinColumns = @JoinColumn(name = "ride_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+    private Set<User> passengers = new HashSet<>();
+
+    private String paradas;
+    private int lugaresDisponiveis;
+
+    private LocalDateTime horarioPartida;
+    private LocalDateTime horarioChegada;
+}
