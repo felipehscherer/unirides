2917737d630bb07c17b583ab7466a8f1719322ef
montanha <diogokmgiordano@gmail.com>
2024-11-04 17:30:30 -0300

    Atualizacao no construtor de request dto do Motorista

--- login-auth-api/src/main/java/br/com/unirides/loginauthapi/dto/driver/DriverRequestDTO.java
+++ login-auth-api/src/main/java/br/com/unirides/loginauthapi/dto/driver/DriverRequestDTO.java
@@ -1,5 +1,7 @@
 package br.com.unirides.loginauthapi.dto.driver;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
@@ -7,10 +9,13 @@ import java.time.LocalDate;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class DriverRequestDTO {
     private String email;
     private String numeroCnh;
     private LocalDate dataEmissao;
     private LocalDate dataValidade;
     private String categoria;
+
 }
