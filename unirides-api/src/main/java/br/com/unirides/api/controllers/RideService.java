f12c6731c4297187e7e855ce2239ea570ebe351e
Felipe Dresch <felipe10dresch12@gmail.com>
2024-12-16 01:12:50 -0300

    Refatoração da página de buscar carona e diversos ajustes pequenos

--- unirides-api/src/main/java/br/com/unirides/api/controllers/RideService.java
+++ unirides-api/src/main/java/br/com/unirides/api/controllers/RideService.java
@@ -10,7 +10,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.text.Normalizer;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
